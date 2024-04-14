54b44c808f4de2723cff5073da77a051c60836ac
Hara1dDev <haraldwikhall@gmail.com>
2024-04-14 21:03:05 +0200

    TEST

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Hara1dDev/rlp
+
+go 1.22.1
---
+++ vector.go
@@ -0,0 +1,23 @@
+package main
+
+type Vec2 struct {
+	X float32
+	Y float32
+}
+
+type Vec3 struct {
+	X float32
+	Y float32
+	Z float32
+}
+
+func ProcessVector(vector interface{}) {
+	switch vector.(type) {
+	case Vec2:
+		print(vector, "2")
+	case Vec3:
+		print(vector, "3")
+	default:
+		print("No vector recieved")
+	}
+}
